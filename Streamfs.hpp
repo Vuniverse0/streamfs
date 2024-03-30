27bc3f544e0631b0645bd86e105963ff265800bd
vuniverse <vuniverse@pm.me>
2024-03-30 23:01:49 +0200

    open file mechanics

--- Streamfs.hpp
+++ Streamfs.hpp
@@ -10,13 +10,36 @@
 #include <cstdio>
 #include <cassert>
 
+/* Use document
+ * construct main styles are classic and table, also can be added
+ * any encryption or/and compression, for this use styles like:
+ * table + encrypted // encryption and table.
+ * table + compressed // compression and table.
+ * compressed + encrypted // compressed_encrypted.
+ * table + compressed + encrypted == table_compressed_encrypted
+ * classic style is using by default
+ */
 
 namespace streamfs
 {
-    enum struct Mode{ write, read, free_stream, error};
-    template<Mode t_Mode = Mode::free_stream, char* t_Extension = "streamfs">
+    enum struct Mode{ error, write, read, free_stream };
+    enum Style{
+        classic, table, encrypted, table_encrypted, compressed,
+        table_compressed, compressed_encrypted, table_compressed_encrypted, error
+    };
+
+    static_assert(table + encrypted == table_encrypted);
+    static_assert(table + compressed == table_compressed);
+    static_assert(table + compressed_encrypted == table_compressed_encrypted);
+    static_assert(compressed + encrypted == compressed_encrypted);
+    static_assert(table + compressed + encrypted == table_compressed_encrypted);
+
+    template<Mode t_Mode = Mode::free_stream>
     class File
     {
+        enum struct State{error, ok, process, none,
+                          encrypt_failed, compress_failed,
+                          decrypt_failed, decompress_failed};
         typedef std::int8_t m_return_code_type;
         typedef std::int64_t m_size_type;
         typedef std::FILE* m_file_type;
@@ -24,22 +47,73 @@ namespace streamfs
         typedef char m_buffer_type [];
         typedef char m_info_buffer_type [10];
 
+        /* 1 byte Style
+         * 8 bytes check code
+         *
+         *
+         */
+
         m_file_type m_file = nullptr;
         m_callback_type m_encrypt = nullptr;
         m_callback_type m_compress = nullptr;
-        Mode m_mode = Mode::error;
+        Mode m_mode = t_Mode;
+        Style m_style;
+        State m_state = State::error;
+        std::string_view m_extension;
 
     public:
-        explicit File(const std::string& a_name) :
-        m_mode { t_Mode }
-        { assert(p_open(a_name)); }
+        explicit File(const std::string& a_name,
+                      Style a_style = classic,
+                      std::string_view a_extension = ".streamfs")
+        :m_style {a_style}
+        ,m_extension{a_extension}
+        {
+            if(p_open(a_name+m_extension.data()) != 0){
+                m_mode = Mode::error;
+                return;
+            }
+        }
 
         ~File(){ std::fclose(m_file); }
 
-
         void setEncrypt(m_callback_type callback) { m_encrypt = callback; }
+
         void setCompress(m_callback_type callback) { m_compress = callback; }
-        m_size_type write(){}
+
+        m_size_type write() //returns bytes writen
+        {
+
+        }
+
+        std::string getErrors()
+        {
+            std::string error_message{"\n"};
+            if(m_style == encrypted
+            || m_style == compressed_encrypted
+            || m_style == table_encrypted
+            || m_style == table_compressed_encrypted
+            && m_encrypt == nullptr){
+                error_message += "Style contains encrypt but callback has not setted!\n";
+            }else if(m_state == State::encrypt_failed){
+                error_message += "Encrypt failed\n";
+            }else if(m_state == State::decrypt_failed){
+                error_message += "Decrypt failed\n";
+            }
+
+            if(m_style == compressed
+            || m_style == compressed_encrypted
+            || m_style == table_compressed
+            || m_style == table_compressed_encrypted
+            && m_compress == nullptr){
+                error_message += "Style contains compress but callback has not setted!\n";
+            }else if(m_state == State::compress_failed){
+                error_message += "Compress failed\n";
+            }else if(m_state == State::decompress_failed){
+                error_message += "Decompress failed\n";
+            }
+
+            return std::move(error_message);
+        }
 
     private:
         static m_return_code_type p_check(m_file_type a_file)
@@ -61,21 +135,22 @@ namespace streamfs
             }();
             switch (m_mode) {
                 case Mode::write :
-                    m_file = std::fopen((a_name + "." + t_Extension).c_str(), "wb+");
+                    m_file = std::fopen(a_name.c_str(), "wb+");
                     break;
                 case Mode::read :
                     if ( is_exist ) {
-                        m_file = std::fopen((a_name + "." + t_Extension).c_str(), "rb+");
+                        m_file = std::fopen(a_name.c_str(), "rb+");
                         if(p_check(m_file)) { m_mode = Mode::error; }
                     } else { m_mode = Mode::error; }
                     break;
                 case Mode::free_stream :
-                    m_file = std::fopen((a_name + "." + t_Extension).c_str(), "ab+");
+                    m_file = std::fopen(a_name.c_str(), "ab+");
                     break;
                 default:
                     break;
             }
-            if ( m_file ) return 1;
+            if ( m_file ) return 0;
+            return 1;
         }
     };
 }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-
+    streamfs::File<> file("test_name1", streamfs::table_encrypted);
     return 0;
 }
